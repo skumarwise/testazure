4aae4e9933babfcc878f0ef6c0d0e32efdf3896d
Sandip Das <dasu.sandi@gmail.com>
2022-07-07 15:36:50 +0530

    adding allowed resource types policy

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,10 +1,3 @@
-
-data "azurerm_subscription" "current" {}
-
-data "azurerm_policy_definition" "allowed_locations" {
-  display_name = "Allowed locations"
-}
-
 resource "azurerm_policy_assignment" "example" {
   name                 = "example-policy-assignment"
   scope                = data.azurerm_subscription.current.id
@@ -15,6 +8,12 @@ resource "azurerm_policy_assignment" "example" {
   parameters = file("${path.module}/parameters.json")
 }
 
-output "allowed_locations_id" {
-  value = data.azurerm_policy_definition.allowed_locations.id
+resource "azurerm_policy_assignment" "allowed_resource_types" {
+  name                 = "allowed_resource_types-policy-assignment"
+  scope                = data.azurerm_subscription.current.id
+  #policy_definition_id = "/providers/Microsoft.Authorization/policyDefinitions/e56962a6-4747-49cd-b67b-bf8b01975c4c"
+  policy_definition_id = data.azurerm_policy_definition.allowed_resource_types.id
+  description          = "Allowed Resource Types:Policy Assignment created via an Terraform Test"
+  display_name         = "Allowed Resource Types:Policy Assignment via Terraform"
+  parameters = file("${path.module}/params/allowed_resource_types.json")
 }
---
+++ terraform/output.tf
@@ -0,0 +1,4 @@
+
+output "allowed_locations_id" {
+  value = data.azurerm_policy_definition.allowed_locations.id
+}
