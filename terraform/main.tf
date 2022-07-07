9707cebbcf2373ad73d196a697944f59329283fb
skumarwise <106962642+skumarwise@users.noreply.github.com>
2022-07-07 14:07:48 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,21 +1,16 @@
 
 data "azurerm_subscription" "current" {}
 
+data "azurerm_policy_definition" "allowed_locations" {
+  display_name = "Allowed locations"
+}
+
 resource "azurerm_policy_assignment" "example" {
   name                 = "example-policy-assignment"
   scope                = data.azurerm_subscription.current.id
-  policy_definition_id = "/providers/Microsoft.Authorization/policyDefinitions/e56962a6-4747-49cd-b67b-bf8b01975c4c"
+  #policy_definition_id = "/providers/Microsoft.Authorization/policyDefinitions/e56962a6-4747-49cd-b67b-bf8b01975c4c"
+  policy_definition_id = data.azurerm_policy_definition.allowed_locations.id
   description          = "Policy Assignment created via an Terraform Test"
   display_name         = "Policy Assignment via Terraform"
-
-  
-parameters = file("${path.module}/parameters.json")
-  /*parameters = <<PARAMETERS
-{
-  "listOfAllowedLocations": {
-    "value": [ "australiaeast" ]
-  }
-}
-PARAMETERS
-*/
+  parameters = file("${path.module}/parameters.json")
 }
