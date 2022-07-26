cfcdf7fb471b2ba432089d082bb1d47e8042924d
skumarwise <106962642+skumarwise@users.noreply.github.com>
2022-07-26 21:37:22 +0530

    Delete terraform directory

--- terraform/output.tf
+++
@@ -1,7 +0,0 @@
-
-output "allowed_locations_id" {
-  value = data.azurerm_policy_definition.allowed_locations.id
-}
-output "allowed_resource_types" {
-  value = data.azurerm_policy_definition.allowed_resource_types.id
-}
